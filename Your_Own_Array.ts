af0aef5ff2ae1391aee93e64c9fd46c8dea8aaf3
mirahmad05 <160038951+mirahmad05@users.noreply.github.com>
2024-03-31 23:16:52 +0500

    Delete Your_Own_Array.ts

--- Your_Own_Array.ts
+++
@@ -1,6 +0,0 @@
-let transportation :string[] = ['Motorcycle', 'Car', 'Bike', 'Train'];
-
-for(let i=0; i<transportation.length; i++){
-
-    console.log('I Would like to own a ' + transportation[i])
-}
