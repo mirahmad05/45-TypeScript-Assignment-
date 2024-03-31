5f8f4d4fa9f65f1f58bfba7ee7360d1f7ef1ad98
mirahmad05 <160038951+mirahmad05@users.noreply.github.com>
2024-03-31 23:17:08 +0500

    Delete Store_the_names12.ts

--- Store_the_names12.ts
+++
@@ -1,8 +0,0 @@
-let names : string[] = ['Mir Ahmad','Gul Rehmat','Haji Rehmat','Wali Rehmat','Khowaja Rehmat','Niaz Ahmad'];
-
-let conts message : string = 'Welcome to Governor Sindh Kamran Khan Tessori IT Initiative Courses Class: ';
-
-for(let i=0; i<names.length; i++){
-
-    console.log(message + names[i]);
-}
